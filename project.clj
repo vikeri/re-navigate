8cd5787ab9219e84303fe326e9085621ac4fe531
Viktor Eriksson <viktor.eriksson2@gmail.com>
2017-01-30 16:18:50 +0100

    Got basic example working

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                            [org.clojure/clojurescript "1.9.198"]
+                           [binaryage/devtools "0.9.0"]
                            [reagent "0.6.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
 [re-frame "0.8.0"]]
             :plugins [[lein-cljsbuild "1.1.4"]
@@ -13,39 +14,40 @@
             :aliases {"prod-build" ^{:doc "Recompile code with prod profile."}
                                    ["do" "clean"
                                     ["with-profile" "prod" "cljsbuild" "once" ]]}
-            :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.8"]
-                                            [com.cemerick/piggieback "0.2.1"]]
-                             :source-paths ["src" "env/dev"]
-                             :cljsbuild    {:builds [{:id           "ios"
-                                                      :source-paths ["src" "env/dev"]
-                                                      :figwheel     true
-                                                      :compiler     {:output-to     "target/ios/not-used.js"
-                                                                     :main          "env.ios.main"
-                                                                     :output-dir    "target/ios"
-                                                                     :optimizations :none}}
-                                                     {:id           "android"
-                                                      :source-paths ["src" "env/dev"]
-                                                      :figwheel     true
-                                                      :compiler     {:output-to     "target/android/not-used.js"
-                                                                     :main          "env.android.main"
-                                                                     :output-dir    "target/android"
-                                                                     :optimizations :none}}]}
-                             :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
+            :profiles {:dev  {:dependencies [[figwheel-sidecar "0.5.8"]
+                                             [com.cemerick/piggieback "0.2.1"]]
+                              :source-paths ["src" "env/dev"]
+                              :cljsbuild    {:builds [{:id           "ios"
+                                                       :source-paths ["src" "env/dev"]
+                                                       :figwheel     true
+                                                       :compiler     {:output-to     "target/ios/not-used.js"
+                                                                      :main          "env.ios.main"
+                                                                      ;:preloads      [devtools.preload]
+                                                                      :output-dir    "target/ios"
+                                                                      :optimizations :none}}
+                                                      {:id           "android"
+                                                       :source-paths ["src" "env/dev"]
+                                                       :figwheel     true
+                                                       :compiler     {:output-to     "target/android/not-used.js"
+                                                                      :main          "env.android.main"
+                                                                      :output-dir    "target/android"
+                                                                      :optimizations :none}}]}
+                              :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
                        :prod {:cljsbuild {:builds [{:id           "ios"
                                                     :source-paths ["src" "env/prod"]
-                                                    :compiler     {:output-to     "index.ios.js"
-                                                                   :main          "env.ios.main"
-                                                                   :output-dir    "target/ios"
-                                                                   :static-fns    true
+                                                    :compiler     {:output-to          "index.ios.js"
+                                                                   :main               "env.ios.main"
+                                                                   :output-dir         "target/ios"
+                                                                   :static-fns         true
                                                                    :optimize-constants true
-                                                                   :optimizations :simple
-                                                                   :closure-defines {"goog.DEBUG" false}}}
-                                                   {:id            "android"
+                                                                   :optimizations      :simple
+                                                                   :closure-defines    {"goog.DEBUG" false}}}
+                                                   {:id           "android"
                                                     :source-paths ["src" "env/prod"]
-                                                    :compiler     {:output-to     "index.android.js"
-                                                                   :main          "env.android.main"
-                                                                   :output-dir    "target/android"
-                                                                   :static-fns    true
+                                                    :compiler     {:output-to          "index.android.js"
+                                                                   :main               "env.android.main"
+                                                                   :output-dir         "target/android"
+                                                                   :static-fns         true
                                                                    :optimize-constants true
-                                                                   :optimizations :simple
-                                                                   :closure-defines {"goog.DEBUG" false}}}]}}})
+                                                                   :optimizations      :simple
+                                                                   :closure-defines    {"goog.DEBUG" false}}}]}}})
--- src/re_navigate/ios/core.cljs
+++ src/re_navigate/ios/core.cljs
@@ -10,24 +10,40 @@
 (def text (r/adapt-react-class (.-Text ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
 (def image (r/adapt-react-class (.-Image ReactNative)))
+(def stack-navigator (.-StackNavigator (js/require "react-navigation")))
+(def tab-navigator (.-TabNavigator (js/require "react-navigation")))
 (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
 
 (def logo-img (js/require "./images/cljs.png"))
 
 (defn alert [title]
-      (.alert (.-Alert ReactNative) title))
+  (.alert (.-Alert ReactNative) title))
 
-(defn app-root []
-  (let [greeting (subscribe [:get-greeting])]
-    (fn []
-      [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
-       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
-       [image {:source logo-img
-               :style  {:width 80 :height 80 :margin-bottom 30}}]
-       [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-                             :on-press #(alert "HELLO!")}
-        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+
+(defn resd []
+  [view [text "Hej"] [text "Hej"]])
+
+(defn app-root [{:keys [navigation]}]
+  (js/console.log (.-navigate navigation))
+  (let [navigate (.-navigate navigation)]
+    [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
+     [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} "Hejsan"]
+     [image {:source logo-img
+             :style  {:width 80 :height 80 :margin-bottom 30}}]
+     [touchable-highlight {:style    {:background-color "#999" :padding 10 :border-radius 5}
+                           :on-press #(navigate "Chat" #js{:user "Lucy"})}
+      [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]]))
+
+
+(def app-root-comp (let [comp (r/reactify-component app-root)]
+                     (aset comp "navigationOptions" #js {"title" "Welcome Man"})
+                     comp))
+
+
+
+(def start (r/adapt-react-class (stack-navigator (clj->js {:Home {:screen app-root-comp}
+                                                           :Chat {:screen (r/reactify-component resd)}}))))
 
 (defn init []
-      (dispatch-sync [:initialize-db])
-      (.registerComponent app-registry "ReNavigate" #(r/reactify-component app-root)))
+  (dispatch-sync [:initialize-db])
+  (.registerComponent app-registry "ReNavigate" #(r/reactify-component start)))
